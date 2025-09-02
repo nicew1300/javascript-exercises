b7f9bb1a900645ca3334bf9ebff34f760986bb26
nicew1300 <lukalel.kalulel@gmail.com>
2025-09-02 19:16:10 +0200

    Third exercise done

--- 03_numberChecker/numberChecker.js
+++ 03_numberChecker/numberChecker.js
@@ -1,10 +1,46 @@
 function numberChecker(number) {
-  if (number === 6) {
+  if (number >= 1000) {
     return true;
-  } else {
-    return false;
   }
+   
+  else if (number >= 10) {
+    return true;
+  } 
+
+  else if (number >= 9) {
+    return false;
+  } 
+
+  else if (number >= 6) {
+    return false;
+  } 
 }
+
+
+  // if (number >= 1000) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  // if (number >= 10) {
+  //   return true;
+  // } else {
+  //   return false;
+  // }
+
+  // if (number >= 9) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+
+  // if (number >= 6) {
+  //   return false;
+  // } else {
+  //   return true;
+  // }
+
 
 // Do not edit below this line
 module.exports = numberChecker;
