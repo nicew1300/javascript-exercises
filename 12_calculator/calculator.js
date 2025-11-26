0493ff0c9fb90594981f9cafbb814227f3629519
Luka Stromajer <lukalel.kalulel@gmail.com>
2025-11-26 10:20:55 +0100

    Fix function signatures for subtract, multiply, and power in calculator.js

--- 12_calculator/calculator.js
+++ 12_calculator/calculator.js
@@ -2,24 +2,28 @@ const add = function(a, b) {
   return a + b;
 };
 
-const subtract = function() {
-	
+const subtract = function(a, b) {
+	return a - b;
 };
 
 const sum = function() {
-	
+	add()
 };
 
-const multiply = function() {
-
+const multiply = function(a, b) {
+  return a * b
 };
 
-const power = function() {
-	
+const power = function(a, b) {
+	return a ** b
 };
 
 const factorial = function() {
-	
+  let result = 1;
+  for (let i = 1; i <= arguments[0]; i++) {
+    result *= i;
+  }
+  return result;
 };
 
 // Do not edit below this line
